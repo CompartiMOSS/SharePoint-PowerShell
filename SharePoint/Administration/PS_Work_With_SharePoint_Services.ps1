4fcf000abd583d6b77f766d17888de5eed4042ce
Juan Carlos González <jcgonzalezmartin1978@htomail.com>
2015-02-07 21:14:38 +0100

    SP Site Event Receivers + Work with SP Services
    Added  2 scripts:
    (1) Get all the event receivers in a SharePoint site.
    (2) Wok with SharePoint Services

---
+++ SharePoint/Administration/PS_Work_With_SharePoint_Services.ps1
@@ -0,0 +1,57 @@
+﻿############################################################################################################################################
+# Script that gets, start and stop all the services in a SharePoint Farm.
+# Required Parameters: 
+#    ->$sOperationType: Operation type to be done (Read, Stop, Start).
+############################################################################################################################################
+
+If ((Get-PSSnapIn -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null ) 
+{ Add-PSSnapIn -Name Microsoft.SharePoint.PowerShell }
+
+$host.Runspace.ThreadOptions = "ReuseThread"
+
+#Definition of the function that gets, start and stop all the services in a SharePoint Farm.
+function WorkWithSharePointServices
+{
+    param ($sOperationType)
+    try
+    {
+        $spServices=Get-SPServiceInstance
+        #Reading Operation
+        if($sOperationType -eq "Read"){
+            Write-Host "Getting all the services in the farm" -ForegroundColor Green
+        }
+        #We iterate throught all the services
+        foreach ($spService in $spServices){
+            switch ($sOperationType) 
+            { 
+            "Read" {                
+                Write-Host $spService.TypeName " - Status: " $spService.Status
+                } 
+            "Stop" {
+                Write-Host "Stopping Service " $spService.TypeName -ForegroundColor Blue
+                Stop-SPServiceInstance -Identity $spService -Confirm:$false           
+                }
+            "Start" {
+                Write-Host "Starting Service " $spService.TypeName -ForegroundColor Blue
+                Start-SPServiceInstance -Identity $spService
+            }         
+            default {
+                Write-Host "Requested Operation not valid!!" -ForegroundColor DarkBlue            
+                }
+            }
+        }
+    }
+    catch [System.Exception]
+    {
+        write-host -f red $_.Exception.ToString()
+    }
+}
+
+Start-SPAssignment –Global
+#Calling the function
+WorkWithSharePointServices -sOperationType "Read"
+#WorkWithSharePointServices -sOperationType "Stop"
+#WorkWithSharePointServices -sOperationType "Start"
+Stop-SPAssignment –Global
+
+Remove-PsSnapin Microsoft.SharePoint.PowerShell
---
+++ SharePoint/Audit/PS_GetSite_EventReceivers.ps1
@@ -0,0 +1,50 @@
+﻿############################################################################################################################################
+# Script that gets all the event receivers for a SharePoint site
+# Required Parameters: 
+#    ->$sSiteCollectionUrl: Site Collection Url.
+############################################################################################################################################
+
+If ((Get-PSSnapIn -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null ) 
+{ Add-PSSnapIn -Name Microsoft.SharePoint.PowerShell }
+
+$host.Runspace.ThreadOptions = "ReuseThread"
+
+#Definition of the function that gets all the event receivers for a SharePoint site
+function Get-SiteEventReceivers
+{
+    param ($sSiteCollectionUrl)
+    try
+    {
+        write-Host "Getting all the Event Receivers for $sSiteCollectionUrl" -ForegroundColor Green
+        $spsSite = Get-SPSite -Identity $sSiteCollectionUrl
+        $spwWeb=$spsSite.OpenWeb()
+        $spContentTypes=$spwWeb.ContentTypes
+         #We need to iterate through the $spContentTyoes to get the receivers for each Content Type in the site
+        foreach($spContentType in $spContentTypes){
+            
+            Write-Host "Conent Type: " $spContentType.Name -ForegroundColor Green
+            if($spContentType.EventReceivers -ne $null)
+            {
+                $spContentType.EventReceivers |  Select { $_.Assembly, $_.Class, $_.Type } | Format-Table
+            }else{
+                Write-Host "No Content Types for " $spContentType.Name
+            }
+            Write-Host "-----------------------------------------------------------"            
+        }      
+        #$spwWeb.ContentTypes.EventReceivers |  Select { $_.Assembly, $_.Class, $_.Type } | Format-Table
+        $spwWeb.Dispose()
+        $spsSite.Dispose()
+    }
+    catch [System.Exception]
+    {
+        write-host -f red $_.Exception.ToString()
+    }
+}
+
+Start-SPAssignment –Global
+#Calling the function
+$sSiteCollectionUrl="http://c4968397007/sites/Intranet"
+Get-SiteEventReceivers -sSiteCollectionUrl $sSiteCollectionUrl
+Stop-SPAssignment –Global
+
+Remove-PsSnapin Microsoft.SharePoint.PowerShell
